a1afca8a78156892e901e896d8cfd4be3396f57c
Alexander Scheel <alex.scheel@hashicorp.com>
2022-10-28 11:49:16 -0400

    FIXUP command/healthcheck/pki.go
    Signed-off-by: Alexander Scheel <alex.scheel@hashicorp.com>

--- command/healthcheck/pki.go
+++ command/healthcheck/pki.go
@@ -1,23 +1,32 @@
 package healthcheck
 
 import (
+	"bytes"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"strings"
 	"time"
 
 	"github.com/hashicorp/vault/sdk/logical"
+
+	"github.com/hashicorp/go-secure-stdlib/parseutil"
 )
 
 type CAValidityPeriod struct {
-	Enabled               bool
+	Enabled bool
+
 	RootExpiries          map[ResultStatus]time.Duration
 	IntermediateExpieries map[ResultStatus]time.Duration
+
+	Issuers map[string]*x509.Certificate
 }
 
 func NewCAValidityPeriodCheck() Check {
 	return &CAValidityPeriod{
-		RootExpiries: make(map[ResultStatus]time.Duration, 3),
-    	IntermediateExpieries: make(map[ResultStatus]time.Duration, 3),
+		RootExpiries:          make(map[ResultStatus]time.Duration, 3),
+		IntermediateExpieries: make(map[ResultStatus]time.Duration, 3),
+		Issuers:               make(map[string]*x509.Certificate),
 	}
 }
 
@@ -37,9 +46,6 @@ func (h *CAValidityPeriod) DefaultConfig() map[string]interface{} {
 }
 
 func (h *CAValidityPeriod) LoadConfig(config map[string]interface{}) error {
-	h.RootExpiries = make(map[ResultStatus]time.Duration, 3)
-	h.IntermediateExpieries = make(map[ResultStatus]time.Duration, 3)
-
 	parameters := []string{
 		"root_expiry_critical",
 		"intermediate_expiry_critical",
@@ -49,9 +55,9 @@ func (h *CAValidityPeriod) LoadConfig(config map[string]interface{}) error {
 		"intermediate_expiry_informational",
 	}
 	for _, parameter := range parameters {
-		name_split := strings.Split(parameter, ",")
+		name_split := strings.Split(parameter, "_")
 		if len(name_split) != 3 || name_split[1] != "expiry" {
-			return fmt.Errorf("bad parameter: %v", parameter)
+			return fmt.Errorf("bad parameter: %v / %v / %v", parameter, len(name_split), name_split[1])
 		}
 
 		status, present := NameResultStatusMap[name_split[2]]
@@ -64,7 +70,7 @@ func (h *CAValidityPeriod) LoadConfig(config map[string]interface{}) error {
 			return fmt.Errorf("parameter not present in config; Executor should've handled this for us: %v", parameter)
 		}
 
-		value, err := time.ParseDuration(value_raw.(string))
+		value, err := parseutil.ParseDurationSecond(value_raw)
 		if err != nil {
 			return fmt.Errorf("failed to parse parameter (%v=%v): %w", parameter, value_raw, err)
 		}
@@ -99,15 +105,63 @@ func (h *CAValidityPeriod) FetchResources(e *Executor) error {
 	}
 
 	for _, issuer := range issuersRet.ParsedCache["issuers"].([]string) {
-		_, err = e.FetchIfNotFetched(logical.ReadOperation, "/{{mount}}/issuer/"+issuer)
+		issuerRet, err := e.FetchIfNotFetched(logical.ReadOperation, "/{{mount}}/issuer/"+issuer+"/json")
 		if err != nil {
 			return err
 		}
+
+		if len(issuerRet.ParsedCache) == 0 {
+			// Need to parse out the issuer from its PEM format.
+			pemBlock, _ := pem.Decode([]byte(issuerRet.Response.Data["certificate"].(string)))
+			if pemBlock == nil {
+				return fmt.Errorf("failed to parse issuer's PEM: %v", issuer)
+			}
+
+			cert, err := x509.ParseCertificate(pemBlock.Bytes)
+			if err != nil {
+				return fmt.Errorf("failed to parse certificate for issuer (%v): %w", issuer, err)
+			}
+
+			issuerRet.ParsedCache["certificate"] = cert
+		}
+
+		h.Issuers[issuer] = issuerRet.ParsedCache["certificate"].(*x509.Certificate)
 	}
 
 	return nil
 }
 
-func (h *CAValidityPeriod) Evaluate(e *Executor) ([]Result, error) {
-	return nil, nil
+func (h *CAValidityPeriod) Evaluate(e *Executor) (results []*Result, err error) {
+	now := time.Now()
+
+	for name, cert := range h.Issuers {
+		var ret Result
+		ret.Status = ResultOK
+		ret.Endpoint = "/{{mount}}/issuer/" + name
+		ret.Message = fmt.Sprintf("Issuer (%v)'s expiration (%v) is OK", name, cert.NotAfter.Format("2006-01-02"))
+
+		hasSelfReference := bytes.Equal(cert.RawSubject, cert.RawIssuer)
+		isSelfSigned := cert.CheckSignatureFrom(cert) == nil
+		isRoot := hasSelfReference && isSelfSigned
+
+		for _, criticality := range []ResultStatus{ResultCritical, ResultWarning, ResultInformational} {
+			var d time.Duration
+			if isRoot {
+				d = h.RootExpiries[criticality]
+			} else {
+				d = h.IntermediateExpieries[criticality]
+			}
+
+			windowExpiry := now.Add(d)
+			if cert.NotAfter.Before(windowExpiry) {
+				ret.Status = criticality
+				ret.Message = fmt.Sprintf("Issuer (%v)'s expiration (%v) is within the %v window of %v", name, cert.NotAfter.Format("2006-01-02"), ResultStatusNameMap[criticality], d.String())
+				break
+			}
+		}
+
+		results = append(results, &ret)
+	}
+
+	return
 }
